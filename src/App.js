2fc2652f42ec81512fe4c60666b2c1ee950ee46f
Sunil Harde <sunilharde10@gmail.com>
2024-05-20 21:10:10 +0530

    card Border And Input

--- src/App.js
+++ src/App.js
@@ -65,146 +65,148 @@ function App() {
 
 
   const getBackgroundVideoSource = (conditionText) => {
-    return weatherConditionVideos[conditionText] || 'default-video-source.mp4';
+    return weatherConditionVideos[conditionText] || 'https://cdn.pixabay.com/video/2019/10/24/28236-368501609_tiny.mp4';
   };
 
 
   return (
 
 
-    <div className='main '>
+    <div className=' '>
+      <div className='main'>
 
-      <div className='video-background'>
-        {apiData && (
-          <video autoPlay loop muted>
-            <source src={getBackgroundVideoSource(apiData.current.condition.text)} type='video/mp4' />
-          </video>
-        )}
-      </div>
+        <div className='video-background'>
+          {apiData && (
+            <video autoPlay loop muted>
+              <source src={getBackgroundVideoSource(apiData.current.condition.text)} type='video/mp4' />
+            </video>
+          )}
+        </div>
 
-      <div className='d-flex flex-column bg-transparent justify-content-center align-items-center mt-4'>
-        <div className='d-flex search  align-items-center mb-3 rounded-5  ' >
-          
-          <input type="text" className='rounded-5 py-2 text-capitalize fs-3 bg-transparent ms-4 '
-            style={{ outline: "none", border:"0", width:"10rem", marginLeft:'1rem' }}
-            onChange={(e) => setLocation(e.target.value)} value={location}
-            onKeyDown={enter}
-            placeholder='City Name'
-          />
+        <div className='d-flex flex-column bg-transparent justify-content-center align-items-center mt-4'>
+          <div className='d-flex search  align-items-center mb-3 rounded-5  ' >
+
+            <input type="text" className='rounded-5 py-2 text-capitalize fs-3 bg-transparent ms-4 '
+              style={{ outline: "none", border: "0", marginLeft: '1rem' }}
+              onChange={(e) => setLocation(e.target.value)} value={location}
+              onKeyDown={enter}
+              placeholder='City Name'
+            />
 
 
 
-          <FontAwesomeIcon icon={faSearch}
-            className='fasearch fs-2 mt-1 ' role="button"
-            onClick={click}
-          />
+            <FontAwesomeIcon icon={faSearch}
+              className='fasearch fs-2 mt-1 ' role="button"
+              onClick={click}
+            />
+
+
+          </div>
+
+          {
+            apiData && (
+
+              <>
+
+                <div className='card card1  m-2 mb-5 rounded-5 -5' >
+
+                  <div className="card-header">
+                    <div className='d-flex align-items-center '>
+
+                      <FontAwesomeIcon icon={faLocation} className='fs-2' />
+                      <h4 className='mt-2 ms-4 '>{apiData.location.name}, {apiData.location.region}, {apiData.location.country}</h4>
+
+                    </div>
+
+                  </div>
+
+                  <div className='card-body  row' >
+                    <div className='col-6'>
+                      <h1 className='ms-2 mt-5 cell '> {apiData.current.temp_c}°</h1>
+                      {/* <h3 className='me-2'>F : {apiData.current.temp_f}</h3> */}
+                    </div>
+
+                    <div className='col-6 '>
+                      <img src={apiData.current.condition.icon} alt="Weather Icon" className='ms-4 h-50 w-50 ' style={{ marginTop: "-1rem", marginBottom: "-2rem" }} />
+                      <h2 className='ms-4 mt-5' style={{ marginBottom: "-1rem" }}> {apiData.current.condition.text}</h2>
+                    </div>
+                  </div>
+
+                  <div className="card-footer">
+                    <div className='d-flex align-items-center '>
+                      <FontAwesomeIcon icon={faClock} className='fs-2' />
+                      <h4 className='mt-2 ms-4 '>{apiData.location.localtime} </h4>
+                    </div>
+                  </div>
+
+
+                </div>
+
+                <div className='container'>
+                  <div className='row'>
+
+                    <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
+                      <div className="card rounded-5  " >
+                        <div className="card-header text-center">
+                          <h4>Humidity</h4>
+                        </div>
+
+                        <div className='card-body d-flex justify-content-between align-items-center'>
+                          <FontAwesomeIcon icon={faDroplet} className='fs-2' />
+                          <h2>{apiData.current.humidity}%</h2>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
+                      <div className="card rounded-5  " >
+                        <div className="card-header text-center">
+                          <h4>Wind</h4>
+                        </div>
+
+                        <div className='card-body d-flex justify-content-between align-items-center'>
+                          <FontAwesomeIcon icon={faWind} className='fs-2' />
+                          <h2>{apiData.current.wind_kph} kph</h2>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
+                      <div className="card rounded-5  " >
+                        <div className="card-header text-center">
+                          <h4>Wind Direction</h4>
+                        </div>
+
+                        <div className='card-body d-flex justify-content-between align-items-center'>
+                          <FontAwesomeIcon icon={faFlag} className='fs-2' />
+                          <h2>{apiData.current.wind_dir}</h2>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
+                      <div className="card rounded-5  " >
+                        <div className="card-header text-center">
+                          <h4>Air Pressure</h4>
+                        </div>
+
+                        <div className='card-body d-flex justify-content-between align-items-center'>
+                          <img className='fs-1' style={{ height: "40px", width: "40px", color: "white" }} src={airper} alt="" />
+
+                          <h2>{apiData.current.pressure_mb}mb</h2>
+                        </div>
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </>
+
+            )
+          }
 
 
         </div>
-
-        {
-          apiData && (
-
-            <>
-
-              <div className='card card1  m-2 mb-5 rounded-5 -5' >
-
-                <div className="card-header">
-                  <div className='d-flex align-items-center '>
-
-                    <FontAwesomeIcon icon={faLocation} className='fs-2' />
-                    <h4 className='mt-2 ms-4 '>{apiData.location.name}, {apiData.location.region}, {apiData.location.country}</h4>
-
-                  </div>
-
-                </div>
-
-                <div className='card-body  row' >
-                  <div className='col-6'>
-                    <h1 className='ms-2 mt-5 cell '> {apiData.current.temp_c}°</h1>
-                    {/* <h3 className='me-2'>F : {apiData.current.temp_f}</h3> */}
-                  </div>
-
-                  <div className='col-6 '>
-                    <img src={apiData.current.condition.icon} alt="Weather Icon" className='ms-4 h-50 w-50 ' style={{marginTop:"-1rem", marginBottom:"-2rem" }}/>
-                    <h2 className='ms-4 mt-5' style={{marginBottom:"-1rem" }}> {apiData.current.condition.text}</h2>
-                  </div>
-                </div>
-
-                <div className="card-footer">
-                  <div className='d-flex align-items-center '>
-                    <FontAwesomeIcon icon={faClock} className='fs-2' />
-                    <h4 className='mt-2 ms-4 '>{apiData.location.localtime} </h4>
-                  </div>
-                </div>
-
-
-              </div>
-
-              <div className='container'>
-                <div className='row'>
-
-                  <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
-                    <div className="card rounded-5  " >
-                      <div className="card-header text-center">
-                        <h4>Humidity</h4>
-                      </div>
-
-                      <div className='card-body d-flex justify-content-between align-items-center'>
-                        <FontAwesomeIcon icon={faDroplet} className='fs-2' />
-                        <h2>{apiData.current.humidity}%</h2>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
-                    <div className="card rounded-5  " >
-                      <div className="card-header text-center">
-                        <h4>Wind</h4>
-                      </div>
-
-                      <div className='card-body d-flex justify-content-between align-items-center'>
-                        <FontAwesomeIcon icon={faWind} className='fs-2' />
-                        <h2>{apiData.current.wind_kph} kph</h2>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
-                    <div className="card rounded-5  " >
-                      <div className="card-header text-center">
-                        <h4>Wind Direction</h4>
-                      </div>
-
-                      <div className='card-body d-flex justify-content-between align-items-center'>
-                        <FontAwesomeIcon icon={faFlag} className='fs-2' />
-                        <h2>{apiData.current.wind_dir}</h2>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
-                    <div className="card rounded-5  " >
-                      <div className="card-header text-center">
-                        <h4>Air Pressure</h4>
-                      </div>
-
-                      <div className='card-body d-flex justify-content-between align-items-center'>
-                        <img className='fs-1' style={{ height: "40px", width: "40px", color: "white" }} src={airper} alt="" />
-
-                        <h2>{apiData.current.pressure_mb}mb</h2>
-                      </div>
-                    </div>
-                  </div>
-
-                </div>
-              </div>
-            </>
-
-          )
-        }
-
-
       </div>
     </div>
   )
