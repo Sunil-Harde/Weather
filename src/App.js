1499354861f808698d2fc5776edfb4b9d7b73193
Sunil Harde <sunilharde10@gmail.com>
2024-06-09 02:40:47 +0530

    Add Wow Animetion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-
 import { React, useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faLocation, faClock, faDroplet, faWind, faFlag } from '@fortawesome/free-solid-svg-icons';
 import airper from './png/icons8-atmospheric-pressure-64.png'
 import Patchylightdrizzle from './Video/Patchy light drizzle.mp4'
-import Wow from "wowjs";
-import "animate.css";
 import './App.css'
 
 
@@ -28,11 +25,7 @@ function App() {
         })
     })
 
-
-
   useEffect(() => {
-    const wow = new Wow.WOW();
-    wow.init();
     api();
     getBackgroundVideoSource("Sunny");
 
@@ -80,7 +73,7 @@ function App() {
 
 
     <div className=' '>
-      <div className='main wow swing' data-wow-duration="1s" data-wow-delay="0s">
+      <div className='main'>
 
         <div className='video-background'>
           {apiData && (
@@ -91,7 +84,7 @@ function App() {
         </div>
 
         <div className='d-flex flex-column bg-transparent justify-content-center align-items-center mt-4'>
-          <div className='d-flex search  align-items-center mb-3 rounded-5 Wow bounce' >
+          <div className='d-flex search  align-items-center mb-3 rounded-5  ' >
 
             <input type="text" className='rounded-5 py-2 text-capitalize fs-3 bg-transparent ms-4 '
               style={{ outline: "none", border: "0", marginLeft: '1rem' }}
@@ -115,10 +108,10 @@ function App() {
 
               <>
 
-                <div className='card card1  m-2 mb-5 rounded-5 -5 wow animate__backInDown' data-wow-duration="1s" data-wow-delay="0s" data-wow-offset="0" >
+                <div className='card card1  m-2 mb-5 rounded-5 -5' >
 
                   <div className="card-header">
-                    <div className='d-flex align-items-center ms-2 '>
+                    <div className='d-flex align-items-center '>
 
                       <FontAwesomeIcon icon={faLocation} className='fs-2' />
                       <h4 className='mt-2 ms-4 '>{apiData.location.name}, {apiData.location.region}, {apiData.location.country}</h4>
@@ -139,7 +132,7 @@ function App() {
                     </div>
                   </div>
 
-                  <div className="card-footer ms-2">
+                  <div className="card-footer">
                     <div className='d-flex align-items-center '>
                       <FontAwesomeIcon icon={faClock} className='fs-2' />
                       <h4 className='mt-2 ms-4 '>{apiData.location.localtime} </h4>
@@ -153,12 +146,12 @@ function App() {
                   <div className='row'>
 
                     <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
-                      <div className="card rounded-5  wow animate__backInLeft" data-wow-duration="1s" data-wow-delay="0s" data-wow-offset="0" >
+                      <div className="card rounded-5  " >
                         <div className="card-header text-center">
-                          <h4 className="wow animate__backInLeft " data-wow-duration="1.3s" >Humidity</h4>
+                          <h4>Humidity</h4>
                         </div>
 
-                        <div className='card-body d-flex justify-content-between align-items-center wow animate__backInLeft ' data-wow-duration="1.8s" data-wow-delay="0s" data-wow-offset="0">
+                        <div className='card-body d-flex justify-content-between align-items-center'>
                           <FontAwesomeIcon icon={faDroplet} className='fs-2' />
                           <h2>{apiData.current.humidity}%</h2>
                         </div>
@@ -166,12 +159,12 @@ function App() {
                     </div>
 
                     <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
-                      <div className="card rounded-5  wow animate__backInLeft" data-wow-duration="1.5s" data-wow-delay="0s" data-wow-offset="0" >
+                      <div className="card rounded-5  " >
                         <div className="card-header text-center">
-                          <h4 className="wow animate__backInLeft " data-wow-duration="1.3s" >Wind</h4>
+                          <h4>Wind</h4>
                         </div>
 
-                        <div className='card-body d-flex justify-content-between align-items-center wow animate__backInLeft ' data-wow-duration="2s" data-wow-delay="0s" data-wow-offset="0">
+                        <div className='card-body d-flex justify-content-between align-items-center'>
                           <FontAwesomeIcon icon={faWind} className='fs-2' />
                           <h2>{apiData.current.wind_kph} kph</h2>
                         </div>
@@ -179,13 +172,12 @@ function App() {
                     </div>
 
                     <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
-                      <div className="card rounded-5  wow animate__backInRight" data-wow-duration="1.5s" data-wow-delay="0s" data-wow-offset="0" >
+                      <div className="card rounded-5  " >
                         <div className="card-header text-center">
-                          <h4 className="wow animate__backInLeft " data-wow-duration="1.3s" >Wind Direction</h4>
-
+                          <h4>Wind Direction</h4>
                         </div>
 
-                        <div className='card-body d-flex justify-content-between align-items-center wow animate__backInRight ' data-wow-duration="1.8s" data-wow-delay="0s" data-wow-offset="0">
+                        <div className='card-body d-flex justify-content-between align-items-center'>
                           <FontAwesomeIcon icon={faFlag} className='fs-2' />
                           <h2>{apiData.current.wind_dir}</h2>
                         </div>
@@ -193,12 +185,12 @@ function App() {
                     </div>
 
                     <div className='col-sm-6 col-lg-3 mt-2 mb-3 '>
-                      <div className="card rounded-5  wow animate__backInRight" data-wow-duration="1s" data-wow-delay="0s" data-wow-offset="0" >
+                      <div className="card rounded-5  " >
                         <div className="card-header text-center">
-                          <h4 className="wow animate__backInLeft " data-wow-duration="1.3s" >Air Pressure</h4>
+                          <h4>Air Pressure</h4>
                         </div>
 
-                        <div className='card-body d-flex justify-content-between align-items-center wow animate__backInRight ' data-wow-duration="2s" data-wow-delay="0s" data-wow-offset="0">
+                        <div className='card-body d-flex justify-content-between align-items-center'>
                           <img className='fs-1' style={{ height: "40px", width: "40px", color: "white" }} src={airper} alt="" />
 
                           <h2>{apiData.current.pressure_mb}mb</h2>
