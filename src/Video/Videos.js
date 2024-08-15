d70a85063a68bc1e2cc010a0396e93f2f9273aeb
Sunil-Harde <158262002+Sunil-Harde@users.noreply.github.com>
2024-08-15 18:58:45 +0530

    Update Videos.js

--- src/Video/Videos.js
+++ src/Video/Videos.js
@@ -6,7 +6,9 @@ const weatherConditionVideos = {
     'Light drizzle': 'https://cdn.pixabay.com/video/2019/10/24/28236-368501609_large.mp4',
     'Partly cloudy': 'https://cdn.pixabay.com/video/2021/02/11/17/29/partly-cloudy-6008138_960_720.mp4',
     "Light rain shower": "https://cdn.pixabay.com/video/2017/07/20/10745-226632884_large.mp4",
-    'Patchy rain nearby': "https://cdn.pixabay.com/video/2023/02/13/150531-798555662_large.mp4",
+    'Patchy rain nearby': "https://cdn.pixabay.com/video/2023/02/13/150531-798555662_large.mp4",  
+    "Light rain": "https://cdn.pixabay.com/video/2017/07/20/10745-226632884_large.mp4",
+
 };
 
 export default weatherConditionVideos
